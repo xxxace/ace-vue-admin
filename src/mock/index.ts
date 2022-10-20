1a191f1c84b360384a9ebd7599a5099d08846b0d
xxxace <731162160@qq.com>
2022-10-20 10:37:13 +0800

    setup mock in both dev and prod

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,7 +1,12 @@
 // 开发环境下引入mock
-if (import.meta.env.VITE_MODE === 'devlopment') {
-    import('./setup').then((res) => {
-        const { setup } = res.default
-        setup()
-    })
-}
+// if (import.meta.env.VITE_MODE === 'devlopment') {
+//     import('./setup').then((res) => {
+//         const { setup } = res.default
+//         setup()
+//     })
+// }
+// 供github-pages使用
+import('./setup').then((res) => {
+    const { setup } = res.default
+    setup()
+})

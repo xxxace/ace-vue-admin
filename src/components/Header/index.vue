638f6f784279687307b04dd474a9394ea575bd0f
xxxace <731162160@qq.com>
2022-12-09 15:43:47 +0800

    add LockScreen

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -10,6 +10,15 @@
                 <SearchModal />
             </li>
             <li>
+                <a-tooltip :content="`点击锁屏`">
+                    <a-button class="nav-btn" type="outline" shape="circle" @click="handleLockScreen">
+                        <template #icon>
+                            <icon-lock />
+                        </template>
+                    </a-button>
+                </a-tooltip>
+            </li>
+            <li>
                 <a-tooltip :content="`点击切换为${isDark ? '亮色' : '暗黑'}模式`">
                     <a-button class="nav-btn" type="outline" shape="circle" @click="handleToggleTheme">
                         <template #icon>
@@ -32,7 +41,7 @@
             <li>
                 <a-dropdown position="br" :trigger="['hover', 'click']">
                     <div class="avatar-wrap">
-                        <a-avatar :size="32" shape="square" src="@/assets/avatar.jpg">
+                        <a-avatar :size="32" shape="square">
                             <img alt="avatar" src="@/assets/avatar.jpg" />
                         </a-avatar>
                     </div>
@@ -41,7 +50,7 @@
                             <template #icon>
                                 <icon-user />
                             </template>
-                            Ace
+                            {{ user.name }}
                         </a-doption>
                         <a-doption>
                             <template #icon>
@@ -60,12 +69,15 @@
                 </a-dropdown>
             </li>
         </ul>
+
+        <LockScreen :locked="isLockScreen" @unlocked="() => (isLockScreen = false)" />
     </header>
 </template>
 <script lang="ts" setup>
-import { computed, inject } from 'vue';
+import { ref, computed, inject } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAppStore, useUserStore } from '@/store';
+import LockScreen from './modules/LockScreen/index.vue';
 import SearchModal from './modules/SearchModal/index.vue';
 import { Modal } from '@arco-design/web-vue';
 import { useToggle, useFullscreen } from '@vueuse/core';
@@ -74,12 +86,15 @@ import useDarkByDefault from '@/hooks/useDarkByDefault'
 const router = useRouter();
 const appStore = useAppStore();
 const userStore = useUserStore();
-
+const locked = localStorage.getItem('locked')
+const user = computed(() => userStore)
 const isDeskTop = computed(() => appStore.appDevice === 'desktop')
 
 const isDark = useDarkByDefault();
 
 const toggleDark = useToggle(isDark);
+
+const isLockScreen = ref(locked ? Boolean(locked) : false);
 
 const handleToggleTheme = () => {
     toggleDark();
@@ -99,6 +114,10 @@ const logout = () => {
             setTimeout(() => router.push({ name: 'login' }), 300);
         }
     })
+}
+
+const handleLockScreen = () => {
+    isLockScreen.value = true
 }
 
 const toggleDrawerMenu = inject('toggleDrawerMenu');
---
+++ src/components/Header/modules/LockScreen/index.vue
@@ -0,0 +1,173 @@
+<template>
+    <Transition name="fade" mode="out-in">
+        <div v-if="props.locked" class="lock-screen" @click.self="() => toggleShowUnlock()">
+            <Transition name="fade" mode="out-in">
+                <div v-if="showUnlock" class="unlock-wrap">
+                    <div class="unlock">
+                        <a-avatar :size="180">
+                            <img alt="avatar" src="@/assets/avatar.jpg" />
+                        </a-avatar>
+                        <div style="font-size: 48px;margin:4px 0 24px 0;">{{ user.name }}</div>
+                        <div class="password" ref="passwordWrapper">
+                            <a-input-password v-model="password" placeholder="密码 123456"
+                                @focus="() => passwordWrapper?.classList.add('foucs')"
+                                @blur="() => passwordWrapper?.classList.remove('foucs')" @press-enter="handleEnter" />
+                            <a-button @click="handleEnter">
+                                <template #icon>
+                                    <icon-arrow-right />
+                                </template>
+                            </a-button>
+                        </div>
+                        <div v-if="isError" class="tips">密码错误</div>
+                    </div>
+                </div>
+                <div v-else class="date-time">
+                    <div class="timer">{{ time }}</div>
+                    <div class="date">{{ date }}，星期{{ weekTrans(week) }}</div>
+                </div>
+            </Transition>
+        </div>
+    </Transition>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref, defineProps, withDefaults } from 'vue'
+import { useNow, useToggle, useDateFormat } from '@vueuse/core'
+import { useUserStore } from '@/store';
+
+export interface LockScreenPorps {
+    locked: boolean
+}
+
+const props = withDefaults(defineProps<LockScreenPorps>(), { locked: false })
+const emit = defineEmits<{
+    (e: 'unlocked'): void
+}>()
+const isError = ref(false);
+const password = ref<string>();
+const passwordWrapper = ref<HTMLDivElement>();
+const userStore = useUserStore()
+const user = computed(() => userStore)
+const [showUnlock, toggleShowUnlock] = useToggle(false)
+const now = useNow()
+const weeks = ['日', '一', '二', '三', '四', '五', '六']
+const time = useDateFormat(now, 'HH : mm')
+const date = useDateFormat(now, 'MM月DD日')
+const week = useDateFormat(now, 'd')
+const weekTrans = (i: string | number) => weeks[Number(i)];
+
+const handleEnter = () => {
+    if (password.value === '123456') {
+        isError.value = false
+        setTimeout(() => {
+            password.value = ''
+            emit('unlocked')
+            toggleShowUnlock()
+        })
+    } else {
+        console.log('密码错误')
+        isError.value = true
+    }
+}
+window.addEventListener('storage', (e) => {
+    if (e.key === 'locked' && e.oldValue === 'true' && e.newValue === null) {
+        localStorage.setItem('locked', 'true')
+    }
+})
+</script>
+
+<style lang="less" scoped>
+.lock-screen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(51, 51, 51, 0.1);
+    z-index: 999999;
+    backdrop-filter: blur(8px);
+    cursor: pointer;
+    user-select: none;
+
+    .unlock-wrap {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(51, 51, 51, 0.1);
+        z-index: 999999;
+        backdrop-filter: blur(4px);
+
+        .unlock {
+            display: flex;
+            flex-flow: column;
+            position: absolute;
+            top: 24%;
+            left: 0;
+            width: 100%;
+            justify-content: center;
+            align-items: center;
+            transform: translate(0);
+            box-sizing: border-box;
+
+            .password {
+                display: flex;
+                box-sizing: border-box;
+                border: 2px solid rgba(255, 255, 255, 0.3);
+
+                .arco-input-wrapper {
+                    border-radius: 0;
+                }
+
+                .arco-input-wrapper:focus-within,
+                .arco-input-wrapper.arco-input-foucs {
+                    border-color: transparent;
+                }
+
+                .arco-btn-secondary[type='button'] {
+                    color: #fff;
+                    background-color: transparent;
+                }
+
+                &.foucs {
+                    border: 2px solid rgba(255, 255, 255, 0.6);
+                }
+            }
+
+            .tips {
+                margin-top: 6px;
+                color: red;
+            }
+        }
+    }
+
+    .date-time {
+        position: absolute;
+        left: 5%;
+        bottom: 10%;
+
+        .timer {
+            font-size: 120px;
+            font-weight: 400;
+        }
+
+        .date {
+            // padding-left: 10px;
+            font-size: 54px;
+            font-weight: 500;
+        }
+    }
+
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>

b6913127a786f14ac81d21f86ceb2e96b5bfadf3
xxxace <731162160@qq.com>
2022-12-09 15:44:09 +0800

    change exception page

--- src/views/exception/403/index.vue
+++ src/views/exception/403/index.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        403
-    </div>
+    <a-result class="result" status="403" subtitle="没有权限访问，请联系管理员。">
+        <template #extra>
+            <a-space>
+                <a-button type="primary" @click="goback">返回</a-button>
+            </a-space>
+        </template>
+    </a-result>
 </template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goback = () => router.back();
+</script>
+
+<style lang="less" scoped>
+.result {
+    margin-top: 20%;
+    height: 100%;
+}
+</style>
--- src/views/exception/404/index.vue
+++ src/views/exception/404/index.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        404
-    </div>
+   <a-result class="result" status="404" subtitle="挖趣！找不到该页面.">
+    <template #extra>
+      <a-space>
+        <a-button type="primary" @click="goback">Back</a-button>
+      </a-space>
+    </template>
+  </a-result>
 </template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goback = () => router.back();
+</script>
+
+<style lang="less" scoped>
+.result {
+    margin-top: 20%;
+    height: 100%;
+}
+</style>

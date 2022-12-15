867eb34ef23358332793924e87bf68123926c048
xxxace <731162160@qq.com>
2022-12-15 11:28:52 +0800

    repack 2022-12-15 11:28

---
+++ dist/assets/setup-58d78311.js
@@ -0,0 +1,1 @@
+import{a$ as t}from"./layout-c1abb15b.js";const o={setup(){t.setup({timeout:"500-1000"})}};export{o as default};
--- dist/assets/setup-e9c3d470.js
+++
@@ -1,1 +0,0 @@
-import{a_ as t}from"./layout-9b91d001.js";const o={setup(){t.setup({timeout:"500-1000"})}};export{o as default};

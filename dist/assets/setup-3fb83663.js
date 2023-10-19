6d34aa69e0b3e428279768e88f83cb3b20e4e5be
xxxace <731162160@qq.com>
2023-10-19 15:59:43 +0800

    repack 2023-10-19 15:59

--- dist/assets/setup-3fb83663.js
+++
@@ -1,1 +0,0 @@
-import{a_ as t}from"./layout-9649e413.js";const o={setup(){t.setup({timeout:"500-1000"})}};export{o as default};
---
+++ dist/assets/setup-62292e13.js
@@ -0,0 +1,1 @@
+import{a_ as t}from"./layout-d1f349db.js";const o={setup(){t.setup({timeout:"500-1000"})}};export{o as default};
